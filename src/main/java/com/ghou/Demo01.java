defe249911cf01301d3b83f7a3ecff64d41e1066
“ogh <ogh@huitone.com>
2021-02-24 10:45:43 +0800

    初次提交

---
+++ src/main/java/com/ghou/Demo01.java
@@ -0,0 +1,12 @@
+package com.ghou;
+
+/**
+ * @author: OHG
+ * @createDate: 2021-02-24
+ * @version: 1.0
+ **/
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello flink....");
+    }
+}
